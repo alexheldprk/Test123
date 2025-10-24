61acf28bb83aa71702854323758162055763a579
Alexander Held <ah@wiko.de>
2025-10-24 13:31:13 +0200

    befüllen von formular funktioniert. OHNE ajax/jquery funktion

---
+++ test123/Scripts/People/people.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    $("#addform").submit(function (event) {
+        event.preventDefault();
+
+        var name = $("#nameinput").val();
+
+        $.ajax({
+            url: '/People/Add',
+            type: 'POST',
+            data: { name: name },
+            success: function (newPerson) {
+                $("#peopleTable").append(
+                    "<tr><td>" + newPerson.Id + "<td><td>" + newPerson.Name + "<td><tr>"
+                );
+                $("#nameInput").val("");
+            },
+            error: function () {
+                alert("Fehler beim Hinzufügen!")
+            }
+        });
+    });
+});
