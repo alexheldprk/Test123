f5459336c2668161482dca70fead2c80874d6332
Alexander Held <ah@wiko.de>
2025-10-27 15:37:00 +0100

    komplett funktionsfähig und aufgeräumt !OHNE AJAX!

--- test123/Controllers/HomeController.cs
+++ test123/Controllers/HomeController.cs
@@ -9,45 +9,61 @@ namespace test123.Controllers
 {
     public class HomeController : Controller
     {
-        private static Person[] people = new Person[0];
-        private static int nextId = 1;
+        //private static Person[] people = new Person[0];
+        //private static int nextId = 1;
 
-        public ActionResult Index()
-        {
-            System.Diagnostics.Debug.WriteLine("Aktuelle Anzahl im Array: " + people.Length);
-            return View(people);
-        }
+        //public ActionResult Index()
+        //{
+        //    System.Diagnostics.Debug.WriteLine("Aktuelle Anzahl im Array: " + people.Length);
+        //    return View(people);
+        //}
 
-        [HttpPost]
-        public ActionResult Add(string name)
-        {
-            Person newPerson = null;
-            if (!string.IsNullOrWhiteSpace(name))
-            {
-                newPerson = new Person
-                {
-                    Id = nextId,
-                    Name = name
-                };
+        //[HttpPost]
+        //public ActionResult Add(string name)
+        //{
+        //    Person newPerson = null;
+        //    if (!string.IsNullOrWhiteSpace(name))
+        //    {
+        //        newPerson = new Person
+        //        {
+        //            Id = nextId,
+        //            Name = name
+        //        };
 
-                nextId++;
+        //        nextId++;
 
-                var newArray = new Person[people.Length + 1];
+        //        var newArray = new Person[people.Length + 1];
 
-                for (int i = 0; i < people.Length; i++)
-                {
-                    newArray[i] = people[i];
-                }
+        //        for (int i = 0; i < people.Length; i++)
+        //        {
+        //            newArray[i] = people[i];
+        //        }
 
-                newArray[people.Length] = newPerson;
+        //        newArray[people.Length] = newPerson;
 
-                people = newArray;
+        //        people = newArray;
 
-            }
-            System.Diagnostics.Debug.WriteLine("Neuer eintrag hinzugefügt: " + newPerson.Name);
-            System.Diagnostics.Debug.WriteLine("Gesamtanzahl: " + people.Length);
+        //    }
+        //    System.Diagnostics.Debug.WriteLine("Neuer eintrag hinzugefügt: " + newPerson.Name);
+        //    System.Diagnostics.Debug.WriteLine("Gesamtanzahl: " + people.Length);
 
-            return RedirectToAction("Index");
-        }
+        //    return RedirectToAction("Index");
+        //}
+
+
+
+        //    public ActionResult About()
+        //    {
+        //        ViewBag.Message = "Your application description page.";
+
+        //        return View();
+        //    }
+
+        //    public ActionResult Contact()
+        //    {
+        //        ViewBag.Message = "Your contact page.";
+
+        //        return View();
+        //    }
     }
 }
